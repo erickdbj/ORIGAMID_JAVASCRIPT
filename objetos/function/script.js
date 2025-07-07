58c84484809b3e870298a6b859fb1c0d53334687
Erick Debiazi <erickdebiazi29@gmail.com>
2025-07-07 17:57:08 -0300

    function

--- objetos/function/script.js
+++ objetos/function/script.js
@@ -1,2 +1,29 @@
 const body = document.body;
 body.style.background = '#000';
+
+const perimetro = new Function('lado', 'return lado * 4');
+
+function somar(n1, n2) {
+  return n1 + n2 + '  2';
+}
+
+console.log(somar(3, 3));
+console.log(somar.length)
+console.log(somar.name)
+
+function darOi(nome, idade) {
+  console.log('Oi para voce ' + nome + ' , idade: '+ idade);
+}
+
+darOi.call({}, 'André', 28);
+
+window.marca = 'Kia';
+window.ano = '2015';
+
+function descricaoCarro(velocidade) {
+  console.log(this);
+  console.log(window.marca + " " + this.ano + " " + velocidade);
+}
+
+descricaoCarro.call({marca: 'Kia Cerato', ano: 2019}, 200);
+
