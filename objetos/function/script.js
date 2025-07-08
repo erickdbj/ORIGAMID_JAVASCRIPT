3f2ff4cdeca45d94226536491fadb16536ae26af
Erick Debiazi <erickdebiazi29@gmail.com>
2025-07-08 17:19:04 -0300

    reduce

--- objetos/function/script.js
+++ objetos/function/script.js
@@ -1,122 +1,21 @@
-const body = document.body;
-body.style.background = '#000';
+document.querySelector('body').style.background = '#000';
 
-// const perimetro = new Function('lado', 'return lado * 4');
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('section p');
 
-// function somar(n1, n2) {
-//   return n1 + n2 + '  2';
-// }
+const somaCaracteres = Array.from(paragrafos).reduce((acc, p) => {
+  const caracteres = p.innerText.length;
+  return acc + caracteres;
+}, 0);
 
-// console.log(somar(3, 3));
-// console.log(somar.length)
-// console.log(somar.name)
+console.log(somaCaracteres);
 
-// function darOi(nome, idade) {
-//   console.log('Oi para voce ' + nome + ' , idade: '+ idade);
-// }
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
 
-// darOi.call({}, 'André', 28);
 
-// window.marca = 'Kia';
-// window.ano = '2015';
-
-// function descricaoCarro(velocidade) {
-//   console.log(this);
-//   console.log(window.marca + " " + this.ano + " " + velocidade);
-// }
-
-// descricaoCarro.call({marca: 'Kia Cerato', ano: 2019}, 200);
-
-// const carros = ['Ford', 'Fiat', 'VW'];
-// const frutas = ['Banan', 'Uva', 'Pera'];
-
-// frutas.forEach.call(carros, (carro) => {
-//   console.log(carro);
-// })
-
-// function Dom(seletor) {
-//   this.element = document.querySelector(seletor);
-//   console.log(this.element);
-// }
-
-// Dom.prototype.ativo = function(classe) { //estou atribuindo essa funçao a esse metodo novo(ativo) que estou criando dentro da propriedade prototype
-//   this.element.classList.add(classe); //Estou dando acesso para todos os new Dom.
-// }
-
-// const ul = new Dom('ul');
-// ul.ativo('ativar'); //Consigo fazer isso, pois consigo acessar todos os metodos e etc que estiverem dentro do prototype.
-
-// const li = {
-//   element: document.querySelector('li'),
-// }
-
-// ul.ativo.call(li, 'ativo'); //O objeto que eu passar aqui como sendo o novo para o ativo, deve ser parecido com o objeto original do metodo.
-
-// function Dom(seletor) {
-//   this.element = document.querySelector(seletor);
-//   console.log(this.element);
-// }
-
-// Dom.prototype.ativo = function(classe) {
-//   console.log(this);
-//   this.element.classList.add(classe); 
-// }
-
-// const li = {
-//   element: document.querySelector('li'),
-// }
-
-// Dom.prototype.ativo.call(li, 'ativar');
-
-const frutas = ['Uva', 'Maçã', 'Banana'];
-
-Array.prototype.pop.call(frutas);
-frutas.pop();
-
-const li = document.querySelectorAll('li');
-const arrayFiltro = Array.from(li);
-
-const teste = arrayFiltro.filter((item) => {
-  return item.classList.contains('ativo');
-});
-
-console.log(teste); //Preciso colocar dentro de um const, pois ele tem que armezanar esses valores em algum lugar
-
-const filtro = Array.prototype.filter.call(li, (item) => {
-  return item.classList.contains('ativo');
-})
-
-const numeros = [123, 535, 2343, 533, 6];
-
-console.log(Math.max.apply(null, numeros));
-
-const li2 = document.querySelectorAll('li');
-
-function itemPossuiAtivo(item) {
-  return item.classList.contains('ativo');
-}
-
-const filtro1 = Array.prototype.filter.call(li, itemPossuiAtivo);
-const filtro2 = Array.prototype.filter.apply(li, [itemPossuiAtivo]); //Ambos funcionam da mesma maneira, mas por apply precisar que chame uma array, é só colocar dentro de uma array a função de callback
-
-const $ = document.querySelectorAll.bind(document);
-
-console.log($('li'));
-
-const carro = {
-  marca: 'Ford',
-  ano: 2018,
-  acelerar: function(aceleracao, tempo) {
-    return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
-  }
-}
-
-const honda = {
-  marca: 'Honda',
-}
-
-const acelerarHonda = carro.acelerar.bind(honda, 200, 10);
-
-console.log(acelerarHonda());
-
-console.log(carro.acelerar(200, 20));
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
