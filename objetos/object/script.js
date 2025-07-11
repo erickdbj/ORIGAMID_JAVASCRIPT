a63b2c86c8b68417fce3badf49c468e607e6568b
erickdbj <erickdebiazi29@gmail.com>
2025-07-10 23:35:49 -0300

    object create

--- objetos/object/script.js
+++ objetos/object/script.js
@@ -1,2 +1,21 @@
 const body = document.body;
-body.style.background = '#000';
+body.style.background = '#020';
+
+const pessoa = new Object({
+  nome: 'André',
+})
+
+console.log(pessoa);
+
+const carro = {
+  marca: 'Marca',
+  rodas: 4,
+  acelerar() {
+    return this.marca + ' acelerou';
+  },
+  buzinar() {
+    return this.marca + ' buzinou';
+  },
+}
+
+const honda = Object.create(carro); //O objeto que eu passar dentro do create, vai fazer parte de todo o prototype de honda.
