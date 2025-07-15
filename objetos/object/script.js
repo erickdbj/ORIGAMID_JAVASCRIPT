e165358a957c660def33a6fab365e3a41d24e508
Erick Debiazi <erickdebiazi29@gmail.com>
2025-07-15 17:56:51 -0300

    object prototype

--- objetos/object/script.js
+++ objetos/object/script.js
@@ -57,45 +57,109 @@
 // Object.assign(moto, funcaoAutomovel);
 // Object.assign(carros, funcaoAutomovel);
 
-const moto = {}
+// const moto = {}
 
-Object.defineProperties(moto, {
-  rodas: {
-    // value: 2,
-    configurable: false, // Impede que a propriedade seja redefinida ou removida (Ja começa como sendo falso)
-    // writable: true, //Permite a alteração de valor
-    enumerable: true,
-    get() {
-      return this._rodas;
-    },
-    set(valor) {
-      this._rodas = valor * 4;
-    },
-  }
-});
+// Object.defineProperties(moto, {
+//   rodas: {
+//     // value: 2,
+//     configurable: false, // Impede que a propriedade seja redefinida ou removida (Ja começa como sendo falso)
+//     // writable: true, //Permite a alteração de valor
+//     enumerable: true,
+//     get() {
+//       return this._rodas;
+//     },
+//     set(valor) {
+//       this._rodas = valor * 4;
+//     },
+//   }
+// });
 
-moto.rodas = 5; //Faz o set, como se fosse set(5);
+// moto.rodas = 5; //Faz o set, como se fosse set(5);
 
-console.log(moto.rodas); //Faz o get
+// console.log(moto.rodas); //Faz o get
 
-console.log(Object.getOwnPropertyDescriptors(Array)); //Pegar info de todos os metodos e propriedades do objeto
-console.log(Object.getOwnPropertyDescriptor(Array, 'from')); //Pegar de apenas um metodo ou propriedade
+// console.log(Object.getOwnPropertyDescriptors(Array)); //Pegar info de todos os metodos e propriedades do objeto
+// console.log(Object.getOwnPropertyDescriptor(Array, 'from')); //Pegar de apenas um metodo ou propriedade
 
-const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight');
+// const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight');
 
-console.log(Object.getOwnPropertyNames(Array));
+// console.log(Object.getOwnPropertyNames(Array));
 
-const frutas = ['Banana'];
+// const frutas = ['Banana', 'Uva'];
 
-console.log(Object.getPrototypeOf(frutas)); //É a mesma coisa (Me da o prototipo do construtor de array)
-console.log(Object.getPrototypeOf('')); //Me da o prototipo do construtor de string
-console.log(Array.prototype); //É a mesma coisa
+// console.log(Object.getPrototypeOf(frutas)); //É a mesma coisa (Me da o prototipo do construtor de array)
+// console.log(Object.getPrototypeOf('')); //Me da o prototipo do construtor de string
+// console.log(Array.prototype); //É a mesma coisa
 
-const frutas1 = ['Banana', 'Pera'];
-const frutas2 = ['Banana', 'Pera'];
+// const frutas1 = ['Banana', 'Pera'];
+// const frutas2 = ['Banana', 'Pera'];
 
-const novaFruta = frutas1; //Apenas referencia frutas1
+// const novaFruta = frutas1; //Apenas referencia frutas1
 
-novaFruta[0] = 'Uva'; //Altera em frutas1 tbm
+// novaFruta[0] = 'Uva'; //Altera em frutas1 tbm
 
-console.log(Object.is(frutas1, novaFruta));
+// console.log(Object.is(frutas1, novaFruta));
+
+// class Pessoa {
+//   constructor(nome, idade) {
+//     this.nome = nome;
+//     this.idade = idade;
+//   }
+
+//   apresentar() {
+//     console.log(this, this.nome)
+//     return `Olá, sou ${this.nome}`;
+//   }
+// }
+
+// const maria = new Pessoa('Maria', 25);
+// //Assim funciona o new por tras dos panos
+// //Cria um objeto vazio
+// //Define o prototipo como pessoa.prototype
+// //Executa Pessoa.call(maria, 'Maria', 25)
+// //Retorna esse objeto
+// const joao = new Pessoa('Joao', 23);
+
+// const carro = {
+//   marca: 'Ford',
+//   ano: 2018,
+// }
+
+// Object.freeze(carro); //Impede alteração e inclusão de propriedades.
+
+// carro.marca = 'Honda';
+
+// console.log(carro);
+
+// Object.seal(carro); //Permite alterar, mas não permite criar novas propriedades. (Não permite deletar tbm)
+// carro.portas = 4;
+
+// Object.preventExtensions(carro); //Previne que eu adicione novas propriedades, mas permite que eu exclua antigas.
+
+// console.log(Object.isFrozen(carro));
+// console.log(Object.isSealed(carro));
+// console.log(Object.isExtensible(carro)); //Se eu prevenir ele da false, se não da true.
+
+// // frutas.map(); //Tenho map, pois é uma array
+// console.log(frutas.hasOwnProperty('map')); //False
+// //pois essa propriedade map é criada em:
+// console.log(Array.prototype.hasOwnProperty('map')); //True
+// console.log(Array.hasOwnProperty('map')); //False
+// //Pois é de Array.prototype e não só de array.
+
+const frutas = ['Banana', 'Uva'];
+
+const somar = function(a, b) {
+  return a + b;
+}
+
+const carro = {
+  marca: 'Ford',
+}
+
+console.log(frutas);
+
+console.log(frutas.toString());
+ //Aqui não consigo verificar corretamente o tipo do objeto pois dentro do prototype de array existe um toString, entao ele chama esse toString, antes do toString do Object.
+
+Object.prototype.toString.call(frutas); //Para verificar entao o tipo de array, eu faço dessa meneira. Utilizo o call para copiar 
