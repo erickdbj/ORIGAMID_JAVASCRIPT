644ca8a41ecc0fb80140e76d15a46bc1e5f33923
Erick Debiazi <erickdebiazi29@gmail.com>
2025-07-29 17:45:38 -0300

    setInterval tests

--- efeitos-no-dom/settimeout-e-setinterval/script.js
+++ efeitos-no-dom/settimeout-e-setinterval/script.js
@@ -2,4 +2,15 @@ function espera(texto) {
   console.log(texto);
 }
 
-setTimeout(espera, 1000, 'Passou 1s');
+setTimeout(espera, 1000, "Passou 1s");
+
+const body = document.body;
+body.style.background = "blue";
+
+setInterval(() => {
+  body.style.background = "red";
+}, 2000);
+
+if (body.style.background !== "blue") {
+  clearInterval(fundoVermelho);  
+}
