7d0d5fa5d60571ee601c820c9065ef778e452c9a
erickdbj <erickdebiazi29@gmail.com>
2025-08-04 20:16:29 -0300

    horarioFuncionamento

--- efeitos-no-dom/date-object/js/modules/funcionamento.js
+++ efeitos-no-dom/date-object/js/modules/funcionamento.js
@@ -1,20 +1,38 @@
 export default function initFuncionamento() {
-  
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+
+  console.log(horarioSemana);
+
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+
+  // const teste = [1, 2, 3, 4, 5].indexOf(4);
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+
+  const horarioAberto =
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
 }
 
-const agora = new Date();
-console.log(agora.getDay()); //-- 4 (Quinta) 0-6 (domingo-sexta)
-console.log(agora.getMonth()); //-- 6 (Julho) 0-11 (meses)
+// const agora = new Date();
+// console.log(agora.getDay()); //-- 4 (Quinta) 0-6 (domingo-sexta)
+// console.log(agora.getMonth()); //-- 6 (Julho) 0-11 (meses)
 
-const futuro = new Date('Dec 31 2025 23:59');
+// const futuro = new Date('Dec 31 2025 23:59');
 
-console.log(futuro);
+// console.log(futuro);
 
-function transformarDias(tempo) {
-  return tempo / (24 * 60 * 60 * 1000);
-}
+// function transformarDias(tempo) {
+//   return tempo / (24 * 60 * 60 * 1000);
+// }
 
-const diasAgora = transformarDias(agora.getTime()); 
-const diasFuturo = transformarDias(futuro.getTime()); 
+// const diasAgora = transformarDias(agora.getTime());
+// const diasFuturo = transformarDias(futuro.getTime());
 
-console.log(Math.floor(diasFuturo - diasAgora));
+// console.log(Math.floor(diasFuturo - diasAgora));
